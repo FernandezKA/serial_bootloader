94731155de2f040f90b8979acd6154eb330414e7
Fernandez K.A <fernandes.kir@yandex.ru>
2021-08-18 15:59:01 +0300

    Write main semantic, untested version

--- src/main.c
+++ src/main.c
@@ -4,7 +4,9 @@ uint8_t u8CountBlock = 0U;
 uint8_t u8CountRecieve = 0U;
 uint8_t u8BlockCRC = 0xFFU;
 uint8_t u8CRC = 0xFF;
-uint8_t u8ACKRequest = 0xABU;
+uint8_t u8rCRC = 0xFF;
+uint8_t u8ACK = 0xABU;
+uint8_t u8NACK = 0xAA;
 uint32_t u32ProgAddr = 0x9000;
 int SystemInit(void)
 {
@@ -20,7 +22,7 @@ void main(void)
 {
 	SystemInit();
 	while (1){
-          GPIOB->ODR^=(1<<5);
+          //GPIOB->ODR^=(1<<5);
           if((BOOT_PORT->IDR & BOOT_PIN) == BOOT_PIN){//without bootloader
             UART1->DR = 0x00;
           }else{//with bootloader
@@ -32,15 +34,25 @@ void main(void)
               asm("nop");//if status is complete, disable IRQ and check CRC
             }
             asm("SIM");
-            u8CRC = u8UART_RecieveNoIRQ();//Recieve CRC         
-            //TODO add check CRC
-            if(u8CountRecieveBlock < u8SoftSize){//Increment block, write flash
-              u8CountRecieveBlock++;
-              FLASH_Unlock(FLASH_MEMTYPE_PROG);
-              FLASH_ProgramBlock(((u32ProgAddr - 0x8000)/64) + u8CountRecieveBlock, FLASH_MEMTYPE_PROG, FLASH_PROGRAMMODE_STANDARD, RXBuff);
-              FLASH_Lock(FLASH_MEMTYPE_DATA);
+            u8rCRC = u8UART_RecieveNoIRQ();//Recieve CRC        
+            if(u8rCRC == u8CRC){
+             vUART_Transmit(u8ACK);
+             u8CountRecieveBlock++;
+             FLASH_Unlock(FLASH_MEMTYPE_PROG);
+             FLASH_ProgramBlock(((u32ProgAddr - 0x8000)/64) + u8CountRecieveBlock, FLASH_MEMTYPE_PROG, FLASH_PROGRAMMODE_STANDARD, RXBuff);
+             FLASH_Lock(FLASH_MEMTYPE_DATA);
             }
             else{
+              vUART_Transmit(u8NACK);
+              for(uint8_t i = 0; i < 64; i++){//clear Buff
+                RXBuff[i] = 0;
+              }
+            }
+    //if no all program block is recieved, write next block
+            if(u8CountRecieveBlock == u8SoftSize){//Increment block, write flash
+              vUART_Transmit(u8ACK);
+            }
+            else{//If all of software block is recieved, write last data and send respond
               
             }
           }
