c362e7d4ce91abd17c27db4e5fb9db06b0732d46
Fernandez K.A <fernandes.kir@yandex.ru>
2021-09-03 13:05:45 +0300

    Untested implementation of bootloader semantics

--- src/main.c
+++ src/main.c
@@ -36,27 +36,58 @@ void main(void)
     else
     { //with bootloader
       while(1){//Check for new data
+        uint8_t SoftSize = 0;
+        uint8_t u8CountRecieve = 0;
         bool RecieveSoftware = FALSE;
-        if(!RecieveSoftware){
+        if(!RecieveSoftware){//Detect device action
           uint8_t Request = 0x00;
           Request = u8UART_RecieveNoIRQ();
           switch(Request){
           case 0x01://Detect device
-            
+            vUART_Transmit(u8ACK);
             break;
           case 0x02://Check boot version
-            
+            vUART_Transmit(u8BootVersion);
             break;
           case 0x03://Check soft version
-            
+            vUART_Transmit(u8SoftVersion);
             break;
           case 0x04://Begin soft recieve
-            
+            vUART_Transmit(u8ACK);
+            SoftSize = u8UART_RecieveNoIRQ();
+            vUART_Transmit(u8ACK);
+            RecieveSoftware = TRUE;
             break;
           }
         }
-        else{
-          
+        else{//Recieve software and write flash action
+          asm("RIM");
+          while(RecieveSoftware){
+            if(eBuffState == complete){
+              uint8_t u8dCRC = 0xFF;
+              uint8_t u8CRC = 0xFF;
+              for(uint8_t i = 0; i < 64; ++i){
+                u8dCRC^=RXBuff[i]; 
+              }
+              u8CRC = u8UART_RecieveNoIRQ();
+              if(u8CRC == u8dCRC){
+                ++u8CountRecieve;
+                vUART_Transmit(u8ACK);
+              }
+              else{
+                vUART_Transmit(u8NACK);
+                for(uint8_t i = 0; i < 64; ++i){//Clear buffer
+                  RXBuff[i] = 0x00;
+                }
+              }
+              if(u8CountRecieve == SoftSize){
+                vUART_Transmit(u8ACK);
+                RecieveSoftware = FALSE;
+                asm("SIM");
+                asm("jp 0x9000");
+              }
+            }
+          }
         }
       }
     }
